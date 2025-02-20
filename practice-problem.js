5d50e865e72238005324ec5425e8580ff0d9c360
Brooke <barrel.babe16@gmail.com>
2025-02-19 20:32:50 -0600

    Update practice-problem.js to practice problems page

--- practice-problem.js
+++ practice-problem.js
@@ -1,4 +1,10 @@
 // You are throwing a party and have a list of attendees.
+var myList = ["Sara", "John", "Mary", "Marie", "Joe"];
 // Someone new arrives—update your list.
+myList.push(["Larry"]);
+console.log(myList);
 // Unfortunately, the last person on your list had to cancel—adjust your list accordingly.
+myList.pop("Joe");
+console.log(myList);
 // Finally, check who is still attending and print the updated list.
+console.log(myList);
